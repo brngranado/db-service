19bbe1312a6d9873e8ce24aa3d0b1bab5a9b1280
Bryan Granado <68772240+brngranado@users.noreply.github.com>
2025-04-28 15:56:47 -0400

    Update DataSource.ts

--- src/database/DataSource.ts
+++ src/database/DataSource.ts
@@ -5,7 +5,7 @@ const AppDataSource = new DataSource({
   host: 'localhost',
   port: 3306,
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '1234',
+  password: process.env.DB_PASS || 'admin',
   database: process.env.DB_NAME || 'wallet',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../migrations/**/*{.ts,.js}'],
