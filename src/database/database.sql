612a039487d9e726ef256fe6f09ae279a7fbf847
Bryan Granado <68772240+brngranado@users.noreply.github.com>
2025-04-28 16:02:18 -0400

    Update database.sql

--- src/database/database.sql
+++ src/database/database.sql
@@ -2,9 +2,8 @@
 CREATE DATABASE IF NOT EXISTS wallet;
 USE wallet;
 
--- Tabla 'payments'
-CREATE TABLE IF NOT EXISTS payments (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE payments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     document INT NOT NULL,
     amount FLOAT NOT NULL,
     user_id VARCHAR(255) NOT NULL,
@@ -12,9 +11,8 @@ CREATE TABLE IF NOT EXISTS payments (
     created_at VARCHAR(255) NOT NULL
 );
 
--- Tabla 'wallet'
-CREATE TABLE IF NOT EXISTS wallet (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE wallet (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     document INT NOT NULL,
     amount FLOAT NOT NULL,
     phone VARCHAR(255) NOT NULL,
@@ -22,9 +20,8 @@ CREATE TABLE IF NOT EXISTS wallet (
     type VARCHAR(255) NOT NULL
 );
 
--- Tabla 'users'
-CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     document INT NOT NULL,
     email VARCHAR(255) NOT NULL,
